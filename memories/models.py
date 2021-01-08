382eba9e481c59100296b764f1283f8aeaa50c25
Dima <Dima@mail.ru>
2021-01-08 13:33:38 +0700

    Create models and modify HTMLs for adding memories

--- memories/models.py
+++ memories/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Point(models.Model):
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)  # широта
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)  # долгота
+
+    def __str__(self):
+        return f"{str(self.latitude)}, {str(self.longitude)}"
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=80, blank='False', null='False')
+    point = models.ForeignKey(Point, on_delete=models.CASCADE, blank=True, null=True)
+    comment = models.CharField(max_length=512)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.author}, {self.comment}"
--- memories/views.py
+++ memories/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
+from memories.models import Point, Place
 
 
 def login(request):
+    """
+    View to log in
+    """
     return render(request, 'memories/login.html')
 
 
 @login_required
 def detail(request):
+    """
+    View to show information about User's memories
+    """
     return render(request, 'memories/detail.html')
 
 
 def add(request):
-    return render(request, 'memories/add.html', {})
+    """
+    View to Add memory
+    """
+    if request.method == 'POST':
+        user = request.user
+        point = Point.objects.create(latitude=request.POST['lat'], longitude=request.POST['long'])
+        Place.objects.create(name=request.POST['name'], comment=request.POST['comment'], author=user, point=point)
+        return redirect('detail')
+    return render(request, 'memories/add.html')
